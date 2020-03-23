01577a0b4fe869fd2be28eca4e22282771609195
vikitaotiz <victorotieno598@gmail.com>
2020-03-23 19:42:23 +0300

    Changed Color

--- resources/views/pagez/menu.blade.php
+++ resources/views/pagez/menu.blade.php
@@ -1,73 +1,81 @@
 @include('layouts.frontpage_nav.nav')
 
+<div class="section menu_section">
+    <div class="wrapper menu_wrapper">
+        <div class="menu_nav w-nav" data-collapse="medium" data-animation="default" data-duration="400">
+            <div class="menu_nav_sticky">
+                <h1 class="h1">Menu Items</h1>
+                <div class="menu_links_wrap w-nav-menu">
+                    @if ($categorymenus->count() > 0)
+                        @foreach ($categorymenus as $category)
+                           <a href="#{{$category->title}}" class="menu_nav_link">{{$category->title}}</a>
+                        @endforeach
+                    @else
+                        <h3>No Menu Category Item Created.</h3>
+                    @endif
 
-    <div class="section menu_section">
-        <div class="wrapper menu_wrapper">
-            <div class="menu_nav">
-                <div class="menu_nav_sticky">
-                    <h1 class="h1">Menu Category</h1>
-                    <div class="menu_links_wrap">
-                        @if ($categorymenus->count() > 0)
-                            @foreach ($categorymenus as $category)
-                               <a href="#{{$category->title}}" class="menu_nav_link">{{$category->title}}</a>
-                            @endforeach
-                        @else
-                            <h3>No Menu Category Item Created.</h3>
-                        @endif
+                </div>
 
+                <div class="menu-button w-nav-button">
+                    <div class="hamburger_contain">
+                        <div class="hamburger_line"></div>
+                        <div class="hamburger_line shorter_line"></div>
+                        <div class="hamburger_line"></div>
                     </div>
                 </div>
-            </div>
-
-            <div class="menu_contain">
-
-                @if ($categorymenus->count() > 0)
-                    @foreach ($categorymenus as $category)
-                        <div id="{{$category->title}}" class="menu_category_wrapper">
-                            <div class="menu_category_heading">{{$category->title}}</div>
-                            <hr>
-                            <div class="w-layout-grid menu_grid">
-                            @if ($category->menus->count() > 0)
-                                @foreach ($category->menus()->paginate(3) as $menu)
-                                    <div class="menu_item" style="border:1px solid #AB8D69; padding: 2%; border-radius: 3px;">
-                                        <div>
-                                            @if ($menu->image)
-                                                <img src="{{asset('storage/'.$menu->image)}}" alt="{{$menu->title}}" width="95%" height="200">
-                                            @else
-                                                <h5>Image not provided</h5>
-                                            @endif
-                                        </div>
-                                        <div class="name_and_price_wrap">
-                                            <div class="menu_item_name">{{$menu->title}} <small>(single): </small></div>
-                                            <div class="menu_price"><u>Kshs{{$menu->price}}</u></div>
-                                        </div>
-                                        <div class="menu_price"><i><small>( Take away: +Ksh 20)</small></i></div>
-                                        <div class="menu_item_description max_w_400px" style="color: #000;">
-                                            {!!$menu->description!!}
-                                        </div>
-                                        <div>
-                                            <p>
-                                                <a href="tel:{{env('APP_PHONE')}}" class="btn" style="background-color: #AB8D69; color: #fff;">Order Now &#9742;</a>
-                                                <a href="https://api.whatsapp.com/send?phone={{env('APP_WHATSAPP')}}" target="_blank" class="btn" style="background-color: #AB8D69; color: #fff;">
-                                                    <img src="{{asset('tanager/images/whatsapp.png')}}" width="15" alt="" />
-                                                </a>
-                                            </p>
-                                        </div>
-                                    </div>
-                                @endforeach
-                            @else
-                                <h3>No menu items for this category.</h3>
-                            @endif
-                          </div>
-                       </div>
-                    @endforeach
-                @else
-                    <h3>No Menu Category Item Created.</h3>
-                @endif
 
             </div>
         </div>
+        
+        <div class="menu_contain">
+
+            @if ($categorymenus->count() > 0)
+                @foreach ($categorymenus as $category)
+                    <div id="{{$category->title}}" class="menu_category_wrapper">
+                        <div class="menu_category_heading">{{$category->title}}</div>
+                        <hr>
+                        <div class="w-layout-grid menu_grid">
+                        @if ($category->menus->count() > 0)
+                            @foreach ($category->menus as $menu)
+                                <div class="menu_item" style="border:1px solid #AB8D69; padding: 2%; border-radius: 3px;">
+                                    <div>
+                                        @if ($menu->image)
+                                            <img src="{{asset('storage/'.$menu->image)}}" alt="{{$menu->title}}" width="95%" height="200">
+                                        @else
+                                            <h5>Image not provided</h5>
+                                        @endif
+                                    </div>
+                                    <div class="name_and_price_wrap">
+                                        <div class="menu_item_name">{{$menu->title}} <small>(single): </small></div>
+                                        <div class="menu_price"><u>Kshs{{$menu->price}}</u></div>
+                                    </div>
+                                    <div class="menu_price"><i><small>( Take away: +Ksh 20)</small></i></div>
+                                    <div class="menu_item_description max_w_400px" style="color: #000;">
+                                        {!!$menu->description!!}
+                                    </div>
+                                    <div>
+                                        <p>
+                                            <a href="tel:{{env('APP_PHONE')}}" class="button btn" style="background-color: #AB8D69;">Order Now &#9742;</a>
+                                            <a href="https://api.whatsapp.com/send?phone={{env('APP_WHATSAPP')}}" target="_blank" class="button btn" style="background-color: #AB8D69;">
+                                                <img src="{{asset('tanager/images/whatsapp.png')}}" width="15" alt="" />
+                                            </a>
+                                        </p>
+                                    </div>
+                                </div>
+                            @endforeach
+                        @else
+                            <h3>No menu items for this category.</h3>
+                        @endif
+                      </div>
+                   </div>
+                @endforeach
+            @else
+                <h3>No Menu Category Item Created.</h3>
+            @endif
+
+        </div>
     </div>
+</div>
 
 @include('layouts.frontpage_footer.footer_section')
 
